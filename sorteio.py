302da9abee402bb3ee83bb572f0e932c791c4a96
MateusPereiradev <dev.mateuspereira@gmail.com>
2025-11-28 09:09:59 -0300

    usando choice

--- sorteio.py
+++ sorteio.py
@@ -1,6 +1,8 @@
 from random import choice
-sorteio= str(input("Digite aqui o nome dos alunos participantes do sorteio:")).split(",")
-print(f"Referente aos participantes do sorteio, o ganhador é {choice(sorteio)}")
+alunos= str(input('Digite aqui o nome dos alunos participantes:')).strip().split()
+print(f'Referente aos alunos informados o ganhador foi {choice(alunos)}!')
+
+
 '''
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
